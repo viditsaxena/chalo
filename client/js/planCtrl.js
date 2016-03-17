4542f5ea0629c07a5c8e5eb51c8c0830e4c4b232
Vidit Saxena <viditsax@gmail.com>
2016-03-17 11:23:25 -0400

    takes of all the code for Drag and Drop

--- client/js/planCtrl.js
+++ client/js/planCtrl.js
@@ -156,21 +156,7 @@ planCtrl.controller('planController', ['$scope', '$rootScope', '$http', '$cookie
   // *************************************************DRAG AND DROP********************************************************
 
 
-      $scope.models = {
-          selected: null,
-          lists: {"A": [], "B": []}
-      };
 
-      // Generate initial model
-      for (var i = 1; i <= 3; ++i) {
-          $scope.models.lists.A.push({label: "Item A" + i});
-          $scope.models.lists.B.push({label: "Item B" + i});
-      }
-
-      // Model to JSON for demo purpose
-      $scope.$watch('models', function(model) {
-          $scope.modelAsJson = angular.toJson(model, true);
-      }, true);
 
 
   // *************************************************DRAG AND DROP********************************************************
