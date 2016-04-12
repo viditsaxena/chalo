17ca844499cb7cbd6dac67b851485c730a2a6e64
VIDIT SAXENA <ViditSaxena@VIDITs-MBP.fios-router.home>
2016-04-12 18:04:31 -0400

    changes the home page for page piling

--- client/js/app.js
+++ client/js/app.js
@@ -43,8 +43,6 @@ unwanderApp.controller('authController', ['$scope', '$rootScope', '$http', '$coo
     $scope.users = [];
     $scope.newUser = {};
     $scope.logInUser = {};
-
-    // $scope.hasExtension = false;
     $rootScope.customStyle = {};
     $rootScope.customStyle.style = {"color":"black"};
 
@@ -97,20 +95,6 @@ $rootScope.token = $cookies.get('token');
       $location.path('/')
     };
 
-
-
-      // chrome.runtime.sendMessage('lhodlodalcbclpnhghakojgeedmephak', { message: "version" },
-      //   function (reply) {
-      //     console.log($scope.hasExtension);
-      //       if (reply) {
-      //           if (reply.version) {
-      //               if (reply.version >= 0) {
-      //                   $scope.hasExtension = true;
-      //                   console.log($scope.hasExtension);
-      //               }
-      //           }
-      //       }
-      //   });
 
 }]);
 
@@ -197,5 +181,57 @@ unwanderApp.controller('homeController', ['$scope', '$rootScope', '$http', '$coo
     console.log("adding extension");
     chrome.webstore.install();
   };
+
+  angular.element(document).ready(function () {
+
+    /*
+     * Plugin intialization
+     */
+
+
+    $('#pagepiling').pagepiling({
+        verticalCentered: false,
+        css3: false,
+        sectionsColor: ['white', '#E8E8E8', '#f2f2f2', '#EC008C'],
+        onLeave: function (index, nextIndex, direction) {
+
+            //fading out the txt of the leaving section
+            $('.section').eq(index - 1).find('h1, p').fadeOut(700, 'easeInQuart');
+
+            //fading in the text of the destination (in case it was fadedOut)
+            $('.section').eq(nextIndex - 1).find('h1, p').fadeIn(700, 'easeInQuart');
+
+
+            //reaching our last section? The one with our normal site?
+            if (nextIndex == 4) {
+                $('#arrow').hide();
+
+                //fading out navigation bullets
+                $('#pp-nav').fadeOut();
+
+                $('#section4').find('.content').animate({
+                    top: '0%'
+                }, 700, 'easeInQuart');
+            }
+
+            //leaving our last section? The one with our normal site?
+            if (index == 4) {
+                $('#arrow').show();
+
+                //fadding in navigation bullets
+                $('#pp-nav').fadeIn();
+
+                $('#section4 .content').animate({
+                    top: '100%'
+                }, 700, 'easeInQuart');
+            }
+        },
+    });
+
+
+    $('#arrow').click(function () {
+        $.fn.pagepiling.moveSectionDown();
+    });
+    });
 
 }]);
