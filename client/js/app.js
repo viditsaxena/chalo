12bb2718a942aaea762742189da6d5c8bcbbba25
VIDIT SAXENA <ViditSaxena@VIDITs-MBP.fios-router.home>
2016-04-25 13:21:37 -0400

    change my stuff page

--- client/js/app.js
+++ client/js/app.js
@@ -170,6 +170,15 @@ unwanderApp.controller('resourcesController', ['$scope', '$rootScope', '$http', 
 
   }
 
+  angular.element(document).ready(function () {
+$(".btn-pref .btn").click(function () {
+    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+    // $(".tab").addClass("active"); // instead of this do the below
+    $(this).removeClass("btn-default").addClass("btn-primary");
+});
+});
+
+
 
 
 }]);
